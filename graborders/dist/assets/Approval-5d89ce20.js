93d4f6ac2d9c8207fe7e19097115e1c1c53fbe28
Mootassam <Mootassame@gmail.com>
2025-10-14 04:38:00 -0700

    add the build folder

---
+++ graborders/dist/assets/Approval-5d89ce20.js
@@ -0,0 +1,3 @@
+import{j as t}from"./index-0b797a34.js";import{S as r}from"./SubHeader-c0146fec.js";import"./useNotifications-0c4531ea.js";import"./useDispatch-4b077ce7.js";function c(){return t.jsxs("div",{className:"container",children:[t.jsx(r,{title:"Certificate"}),t.jsx("img",{src:"/images/certif.png",alt:"",className:"certif"}),t.jsx("style",{children:`
+            .certif { width:100%}
+            `})]})}export{c as default};
