ff6e3d76cc1e9a59b7f7fbebc032d6cce80e6c4b
Mootassam <Mootassame@gmail.com>
2025-11-09 03:23:47 -0800

    change the URL

--- graborders/src/modules/shared/axios/authAxios.tsx
+++ graborders/src/modules/shared/axios/authAxios.tsx
@@ -6,9 +6,9 @@ import moment from 'moment';
 import AuthToken from 'src/modules/auth/authToken';
 
 const authAxios = Axios.create({
-  // baseURL: "https://nexus-exchange.com/api",
+  baseURL: "https://nexus-exchange.com/api",
 
-  baseURL: "http://localhost:8084/api",
+  // baseURL: "http://localhost:8084/api",
 
   paramsSerializer: function (params) {
     return Qs.stringify(params, {
@@ -44,3 +44,4 @@ authAxios.interceptors.request.use(
 );
 
 export default authAxios;
+
